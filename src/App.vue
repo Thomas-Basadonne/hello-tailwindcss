1624bd194503f95f3324fa786c9760214036c21b
Thomas-Basadonne <thommybasa@gmail.com>
2023-11-11 15:21:47 +0100

    add custom config

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 
 <template>
   <div>
-    <h1 class="text-green-600 bg-slate-400 text-3xl border-b-8 border-red-200">
+    <h1
+      class="text-green-600 bg-slate-400 text-3xl border-b-8 border-red-200 mb-1"
+    >
       Hello Tailwind!
     </h1>
-    <h1 class="text-ghost-100">Hello Tailwind!</h1>
+    <h1 class="text-ghost-100 text-sm md:text-md lg:text-lg">
+      Hello Tailwind!
+    </h1>
   </div>
 </template>
 
