069ed3c512de37d1971bfd68aa39e06714bc908a
Thomas-Basadonne <thommybasa@gmail.com>
2023-11-12 16:27:51 +0100

    spacing + flex

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,30 @@
     </h2>
     <p class="text-lg">Lorem ipsum dolor sit amet.</p>
     <p class="text-base">Lorem ipsum dolor sit amet.</p>
-    <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-    <p class="truncate">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ratione.
-      Totam illum iure laboriosam libero! Illum voluptatum alias praesentium
-      dignissimos recusandae, unde incidunt aliquam consequatur aut eveniet
-      dolore laboriosam laudantium.
+    <p class="text-sm break-all">
+      Loremhvkvkvkhvkhvhvhvhvhvhvhvhvhvhvvhvhvhvhvhvhvhvhvhvhvhhv ipsum dolor
+      sit amet.
     </p>
+    <div class="bg-green-300">
+      <p class="truncate bg-red-400 p-1 mx-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, ratione.
+        Totam illum iure laboriosam libero! Illum voluptatum alias praesentium
+        dignissimos recusandae, unde incidunt aliquam consequatur aut eveniet
+        dolore laboriosam laudantium.
+      </p>
+    </div>
+
+    <div class="flex w-full min-h-24">
+      <div class="w-1/2 bg-yellow-300">sidebar</div>
+      <div class="w-1/2 bg-blue-300">main</div>
+    </div>
+  </div>
+  <div class="Parent">
+    <div>header</div>
+    <div class="flex">
+      <div class="basis-3/4">Main content</div>
+      <div class="basis-1/4">sidebar</div>
+    </div>
   </div>
 </template>
 
